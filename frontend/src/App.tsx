a800b5ee70a580d024bbedf43ed09caafe6fdf1f
gran0la <benfoub@hotmail.com>
2026-06-28 19:30:09 +0100

    changed sidebar bg

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <div id='app' className='w-full h-screen flex bg-[url(/fishies2highres.png)] bg-cover'>
 
-      <div id='left' className='px-12 flex items-center justify-center'>
-        <div id='menu-bar' className='w-20 bg-slate-700/30 backdrop-blur-md rounded-full flex flex-col items-center p-2'>
+      <div id='left' className='px-12 flex items-center justify-center bg-slate-700/30 backdrop-blur-md'>
+        <div id='menu-bar' className='w-20 bg-slate-950/30 backdrop-blur-md rounded-full flex flex-col items-center p-2'>
           <div className='w-16 h-16 my-1 rounded-full bg-white/70'></div>
           <div className='w-16 h-16 my-1 rounded-full bg-white/20'></div>
           <div className='w-16 h-16 my-1 rounded-full bg-white/20'></div>
@@ -14,7 +14,7 @@ function App() {
       </div>
 
       <div id='right' className='flex-1 flex items-center justify-center'>
-        <div id='main-section' className='bg-slate-700/30 backdrop-blur-md w-7/8 h-6/8 rounded-3xl'></div>
+        <div id='main-section' className='bg-slate-700/30 backdrop-blur-md w-7/8 h-7/8 rounded-3xl '></div>
       </div>
 
     </div>
